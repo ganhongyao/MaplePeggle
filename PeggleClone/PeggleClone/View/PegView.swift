a66bdfe9a6997a05276b3ea8093b61b3e549bedc
Hong Yao <ganhongyao@u.nus.edu>
2022-02-23 00:38:20 +0800

    Remove unused rotate icon

--- PeggleClone/PeggleClone/View/PegView.swift
+++ PeggleClone/PeggleClone/View/PegView.swift
@@ -14,41 +14,26 @@ struct PegView: View {
 
     var body: some View {
         ZStack {
-            ZStack {
-                getPegImage(color: pegViewModel.color)
-                    .resizable()
-                    .frame(width: pegViewModel.diameter, height: pegViewModel.diameter)
-                    .rotationEffect(.radians(pegViewModel.facingAngle))
-                    .position(pegViewModel.center)
-
-                if pegViewModel.isSelected {
-                    Circle()
-                        .fill(.blue)
-                        .frame(width: ViewConstants.pegEditingCircleFractionOfDiameter * pegViewModel.diameter,
-                               height: ViewConstants.pegEditingCircleFractionOfDiameter * pegViewModel.diameter)
-                        .position(pegViewModel.center)
-                }
-            }
-            .gesture(DragGesture().onChanged { value in
-                pegViewModel.selectPeg()
-                pegViewModel.movePeg(to: value.location)
-            })
-            .onLongPressGesture {
-                pegViewModel.removePeg()
-            }
+            getPegImage(color: pegViewModel.color)
+                .resizable()
+                .frame(width: pegViewModel.diameter, height: pegViewModel.diameter)
+                .rotationEffect(.radians(pegViewModel.facingAngle))
+                .position(pegViewModel.center)
 
             if pegViewModel.isSelected {
-                Image(systemName: "arrow.clockwise.circle")
-                    .resizable()
-                    .foregroundColor(.blue)
+                Circle()
+                    .fill(.blue)
                     .frame(width: ViewConstants.pegEditingCircleFractionOfDiameter * pegViewModel.diameter,
                            height: ViewConstants.pegEditingCircleFractionOfDiameter * pegViewModel.diameter)
-                    .position(x: pegViewModel.center.x,
-                              y: pegViewModel.center.y + pegViewModel.radius +
-                              ViewConstants.pegEditingCircleFractionOfDiameter * pegViewModel.diameter)
+                    .position(pegViewModel.center)
             }
-
         }
-
+        .gesture(DragGesture().onChanged { value in
+            pegViewModel.selectPeg()
+            pegViewModel.movePeg(to: value.location)
+        })
+        .onLongPressGesture {
+            pegViewModel.removePeg()
+        }
     }
 }
